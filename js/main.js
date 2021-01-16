a164c9a6885cfb1bd4f727bbad7cab2373548672
frida9205 <64855586+frida9205@users.noreply.github.com>
2021-01-16 17:19:16 +0400

    file2

---
+++ js/main.js
@@ -0,0 +1,6 @@
+  window.addEventListener('load', function ShowNavbarRegion() {
+          setTimeout(function () {
+            document.getElementById('open').style.display = 'block';
+          }, 10000);
+        })
+      
